c774cd0a6aaa1476cb4d4abbeb6bb3da4ea1ef36
Muhammad Anas Raza Siddiqui <anasattari48@gmail.com>
2022-10-21 13:59:55 +0500

    cha

--- src/Components/PostBody.jsx
+++ src/Components/PostBody.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import './PostBody.css'
 import axios from 'axios';
 import moment from 'moment';
+
 import { useState, useEffect } from "react";
 const PostBody = () => {
-
+  const [data, setdata] = useState([])
 useEffect(() => {
   const getTrandingNews = ()=>{
     const options = {
@@ -20,6 +21,8 @@ useEffect(() => {
     
     axios.request(options).then(function (response) {
       console.log(response.data);
+      setdata(response.data)
+
     }).catch(function (error) {
       console.error(error);
     });
@@ -43,7 +46,9 @@ useEffect(() => {
   <div className="column">
     <div className="content">
       <img src="/w3images/mountains.jpg" alt="Mountains"  />
-      <h3>My Work</h3>
+       {data.map(eachPost => (
+          <h3 className="post" key={eachPost?.name}></h3>
+          ))}  
       <p  >Pulished on 15-45-2021</p>
       <p>Lorem ipsum dolor sit amet, tempor prodesset eos no. Temporibus necessitatibus sea ei, at tantas oporteat nam. Lorem ipsum dolor sit amet, tempor prodesset eos no.</p>
     </div>
