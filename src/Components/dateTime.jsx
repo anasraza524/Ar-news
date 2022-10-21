70dad3a4dc571e452ecd6df34c8142387155ccc4
Muhammad Anas Raza Siddiqui <anasattari48@gmail.com>
2022-10-21 18:27:36 +0500

    some

--- src/Components/dateTime.jsx
+++ src/Components/dateTime.jsx
@@ -1,38 +1,34 @@
-// import React from 'react'
-// import { useState, useEffect } from "react";
-// import { BsClockHistory  } from "react-icons/bs";
-// import { AiTwotoneCalendar  } from "react-icons/ai";
+import React from 'react'
+import { useState, useEffect } from "react";
+import { BsClockHistory  } from "react-icons/bs";
+import { AiTwotoneCalendar  } from "react-icons/ai";
+import moment from 'moment';
 
+const dateTime = () => {
 
-// const dateTime = () => {
-
-//   const [dateState, setDateState] = useState(new Date());
-//   useEffect(() => {
+ const [dateState, setDateState] = useState()
+  
+// useEffect(() => {
 //          setInterval(() => setDateState(new Date()), 30000);
 //   }, []);
-//   return (
-    
-//             <div className="App">
-//             <AiTwotoneCalendar />
-//             <p>
-//               {' '}
-//               {dateState.toLocaleDateString('en-GB', {
-//                  day: 'numeric',
-//                  month: 'short',
-//                  year: 'numeric',
-//               })}
-//             </p>
-//             <BsClockHistory />
-//             <p>
-//              {dateState.toLocaleString('en-US', {
-//                 hour: 'numeric',
-//                 minute: 'numeric',
-//                 hour12: true,
-//             })}
-//             </p>
-//         </div>
-       
-//   )
-// }
 
-// export default dateTime
+  return (
+    
+            <div className="App">
+            <AiTwotoneCalendar />
+            <p>
+            
+              {
+setDateState( moment(new Date()).format("hh:mm A"))
+              }
+            </p>
+            <BsClockHistory />
+            <p>{
+            
+}</p>
+        </div>
+       
+  )
+}
+
+export default dateTime
